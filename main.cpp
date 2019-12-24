155e01f7f530f946c31ca1c2f2cb6cd157ca0f8b
Jiija <48101424+Jiija@users.noreply.github.com>
2019-12-24 21:58:18 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -1,37 +1,55 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "filters/filters.h"
 #include "png_toolkit.h"
 
-int main( int argc, char *argv[] )
+int main(int argc, char* argv[])
 {
-    // toolkit filter_name base_pic_name sudent_tool student_pic_name limitPix limitMSE
-    // toolkit near test images!
-    try
-    {
-        if (argc != 3)
-            throw "Not enough arguments";
-
-        png_toolkit studTool;
-        studTool.load(argv[1]);
-		image_data image = studTool.getPixelData();
+	// toolkit filter_name base_pic_name sudent_tool student_pic_name limitPix limitMSE
+	// toolkit near test images!
+	//try
+	//{
+		//if (argc != 3)
+			//throw "Not enough arguments";
+	std::string filterName;
+	std::ifstream configStream;
+	size_t u, l, b, r;
+	configStream.open(argv[1]);
+	png_toolkit studTool;
+	studTool.load(argv[2]);
+	image_data image = studTool.getPixelData();
+	while (!configStream.eof())
+	{
+		configStream >> filterName;
+		configStream >> u >> l >> b >> r;
 		if (image.compPerPixel >= 3)
 		{
-			size_t image_size = (size_t)image.w * image.h * image.compPerPixel;
-			unsigned char* p = NULL;
-			for (p = image.pixels; p < image.pixels + image_size / 2; p += image.compPerPixel);
-			for (; p < image.pixels + image_size; p += image.compPerPixel)
-			{
-				*p = 255;
-				*(p + 1) = *(p + 2) = 0;
-			}
+			FilterBase* filter;
+			if (filterName == "Red")
+				filter = new FilterRed();
+			else if (filterName == "Blur")
+				filter = new FilterBlur();
+			else if (filterName == "Edge")
+				filter = new FilterEdge();
+			else if (filterName == "Threshold")
+				filter = new FilterThreshold();
+			else
+				continue;
+			filter->ApplyFilter(image, { l == 0 ? 0 : image.w / l, u == 0 ? 0 : image.h / u, r == 0 ? 0 : image.w / r, b == 0 ? 0 : image.h / b, });
+			delete filter;
 		}
-        studTool.save(argv[2]);
+		filterName.clear();
+	}
+	configStream.close();
+	studTool.save(argv[3]);
 
-    }
-    catch (const char *str)
-    {
-        std::cout << "Error: " << str << std::endl;
-        return 1;
-    }
+	//}
+	//catch (const char* str)
+	//{
+	//	std::cout << "Error: " << str << std::endl;
+	//	return 1;
+	//}
 
-    return 0;
+	return 0;
 }

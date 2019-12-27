6561a569a7e857414b43dfb2a8477b703ec5002f
Jiija <48101424+Jiija@users.noreply.github.com>
2019-12-27 23:48:28 +0300

    Minor fix

--- filterRed.h
+++ filterRed.h
@@ -2,10 +2,8 @@
 #include "filterbase.h"
 class FilterRed : public FilterBase {
 	void ProcessPixel(const image_data& img, stbi_uc* newPixels, size_t i, size_t j, const AreaBounds& bounds) override {
-		int check;
 		stbi_uc* currPixel;
 		currPixel = img.pixels + i * img.w * img.compPerPixel + j * img.compPerPixel;
-		check = (int)currPixel;
 		*currPixel = 255;
 		*(currPixel + 1) = *(currPixel + 2) = 0;
 	}
